e03d9440c203471d6f08c0bdcc930f2d33f9a925
LilChaveZz <59180634+LilChaveZz@users.noreply.github.com>
2025-04-27 15:12:14 -0600

    separated js and css into individual files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.cta').addEventListener('click', function (e) {
+    e.preventDefault();
+    alert('¡Gracias por unirte! Te avisaremos cuando estemos listos para zarpar.');
+});
